0bb8a2add202e851b0336fd205d865b6437a9093
simonw101 <simonhanwilson@icloud.com>
2017-03-30 15:24:23 +0100

    Initialize repo

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var portNumber = 3000;
+
+var middleware = {
+
+  requireAuthentication: function(req, res, next) {
+      console.log("private route hit");
+      next();
+  },
+  logger: function(req, res, next) {
+    var date = new Date().toString();
+    console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
+    next();
+  }
+};
+
+// app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+  res.send('about us');
+});
+
+app.use(express.static('/Users/simonwilson/Dropbox/node/expressPractise/public'));
+
+app.listen(portNumber, function() {
+  console.log("express server started on port " + portNumber);
+});
