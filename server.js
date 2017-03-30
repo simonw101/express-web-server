428f8d6e72ba0a533eaf527eea52a14a3d575fb1
simonw101 <simonhanwilson@icloud.com>
2017-03-30 15:26:00 +0100

    change server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var portNumber = 3000;
 var middleware = {
 
   requireAuthentication: function(req, res, next) {
-      console.log("private route hit");
+      console.log("private route hit ouch...");
       next();
   },
   logger: function(req, res, next) {
