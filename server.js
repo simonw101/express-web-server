f0cdf959fe7967db17a94ee71438685e59eec78e
simonw101 <simonhanwilson@icloud.com>
2017-03-30 17:21:36 +0100

    put middleware into its own seperate file

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 var express = require('express');
 var app = express();
 var portNumber = 3000;
-
-var middleware = {
-
-  requireAuthentication: function(req, res, next) {
-      console.log("private route hit ouch...");
-      next();
-  },
-  logger: function(req, res, next) {
-    var date = new Date().toString();
-    console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
-    next();
-  }
-};
+var middleware = require("./middleware.js");
 
 // app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
